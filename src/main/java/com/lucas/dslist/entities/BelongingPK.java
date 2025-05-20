c2f898f25b1e9b9a1575bb68fdcb33e02b4ae867
Lucas Sgalla <lucasgalla10@gmail.com>
2025-05-20 13:54:16 -0300

    Database seeding update

--- src/main/java/com/lucas/dslist/entities/BelongingPK.java
+++ src/main/java/com/lucas/dslist/entities/BelongingPK.java
@@ -14,7 +14,7 @@ public class BelongingPK {
     private Game game;
 
     @ManyToOne
-    @JoinColumn(name = "gameList_id")
+    @JoinColumn(name = "game_list_id")
     private GameList gameList;
 
     public BelongingPK(){
--- src/main/java/com/lucas/dslist/entities/GameList.java
+++ src/main/java/com/lucas/dslist/entities/GameList.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_gameList")
+@Table(name = "tb_game_list")
 public class GameList {
 
     @Id
